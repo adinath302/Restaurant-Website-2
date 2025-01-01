4de870c92578ff8505e1a95b792e92765d32f068
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-01-01 22:41:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./component/Navbar/Navbar.jsx";
+
+const App = () => {
+  return (
+    <div className="overflow-hidden bg-white2">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
